e3001aebede120f8d604026a06856d379396caff
BuiThienHoang221204 <140410385+BuiThienHoang221204@users.noreply.github.com>
2025-03-15 23:49:28 +0700

    hoang

--- frontend/src/pages/component/Banner.jsx
+++ frontend/src/pages/component/Banner.jsx
@@ -90,19 +90,36 @@ function Banner() {
                     {/* Nút Watch Now - Sử dụng flex và align-items để căn chỉnh icon và text thẳng hàng ngang */}
                     <button className="btn btn-danger">
                         <div className="btn-content">
-                            <FaPlay className="btn-icon" /> 
+                            <FaPlay className="btn-icon" />
                             <span className="btn-text">Watch Now</span>
                         </div>
                     </button>
                     {/* Nút Add to My List - Sử dụng flex và align-items để căn chỉnh icon và text thẳng hàng ngang */}
                     <button className="btn btn-secondary">
                         <div className="btn-content">
-                            <FaPlus className="btn-icon" /> 
+                            <FaPlus className="btn-icon" />
                             <span className="btn-text">Add to My List</span>
                         </div>
                     </button>
                 </div>
             </div>
+            
+            {/* Di chuyển phần hiển thị poster phim ra ngoài banner-content để định vị độc lập */}
+            <div className="movie-poster-container">
+                <div className="movie-poster">
+                    <div className="play-button-overlay">
+                        {/* Sử dụng icon từ react-icons thay vì hình ảnh */}
+                        <FaPlay className="play-icon" />
+                    </div>
+                    {/* Sử dụng hình ảnh từ movie hiện tại */}
+                    <img 
+                        src={movie.image} 
+                        alt={`${movie.title} poster`} 
+                        className="poster-image"
+                    />
+                </div>
+            </div>
+            
             <img src={movie.image} alt={movie.title} className="banner-image" />
             
             {/* Nút điều hướng */}
