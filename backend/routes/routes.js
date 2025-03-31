b8b8224422e72bbe9305fef0f9494815c3b8f1aa
TrinhHao42 <nguoila10112004@gmail.com>
2025-04-01 02:13:50 +0700

    Toan Hao

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -5,7 +5,7 @@ const {signup, login, getAuthStatus, logout} = require("../controller/access.con
 const passport = require("../config/passport.config")
 const { getFilms } = require("../controller/drive.controller")
 const router = express()
-const movies = require("../module/movies.module")
+const movies = require("../module/movie.module")
 
 router.use(passport)
 
